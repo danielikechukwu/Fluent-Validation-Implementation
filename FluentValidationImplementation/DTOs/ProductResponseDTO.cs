63573c1dd0f23a3aae94f2d316c108b64628fc22
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-08-02 16:03:20 +0100

    feat: Add DTOs and validation for product management
    Created `ProductCreateDTO`, `ProductResponseDTO`, and `ProductUpdateDTO` to manage product details.
    
    Implemented `ProductCreateDTOValidator` to enforce validation rules for product creation, including checks for SKU, Name, Price, Discount, ManufacturingDate, ExpiryDate, and Tags.

---
+++ FluentValidationImplementation/DTOs/ProductResponseDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace FluentValidationImplementation.DTOs
+{
+    public class ProductResponseDTO
+    {
+        public int ProductId { get; set; }
+
+        public string SKU { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public int CategoryId { get; set; } 
+
+        public string? Description { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public DateTime ManufacturingDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public List<string>? Tags { get; set; }
+    }
+}
