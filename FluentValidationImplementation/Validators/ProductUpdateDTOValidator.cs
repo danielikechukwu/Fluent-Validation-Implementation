ce18b8eab4f8ae5f731a0dbd7bb537455765c743
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-08-09 05:12:16 +0100

    Add custom validation class for ProductUpdateDTO.

---
+++ FluentValidationImplementation/Validators/ProductUpdateDTOValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using FluentValidationImplementation.DTOs;
+
+namespace FluentValidationImplementation.Validators
+{
+    public class ProductUpdateDTOValidator : AbstractValidator<ProductUpdateDTO>
+    {
+        public ProductUpdateDTOValidator() { 
+
+        }  
+    }
+}
